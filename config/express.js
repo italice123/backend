ec16ca2a0f6127e84427bea37b4388d877f792a8
Pitsanu Limpanachaipornkul <pitsanujill12@gmail.com>
2019-02-14 14:57:03 +0700

    [ok]

--- config/express.js
+++ config/express.js
@@ -1,24 +1,21 @@
-const express = require("express");
-const app = express();
-const bodyParser = require("body-parser");
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
 
-module.exports = function() {
-  const app = express();
-  app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Methods", "*");
-    res.setHeader("Access-Control-Allow-Headers", "*");
-    next();
-  });
+module.exports = function () {
+  app.use((_, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader('Access-Control-Allow-Methods', '*')
+    res.setHeader('Access-Control-Allow-Headers', '*')
+    next()
+  })
+  // configure the app to use bodyParser()
   app.use(
     bodyParser.urlencoded({
       extended: true
     })
-  );
-
-  require("../routes/backend.routes")(app);
-  app.use(bodyParser.json());
-  app.set("views", "./views");
-  app.set("view engine", "ejs");
-  return app;
-};
+  )
+  app.use(bodyParser.json())
+  require('../routes/backend.routes')(app)
+  return app
+}
--- routes/backend.routes.js
+++ routes/backend.routes.js
@@ -1,19 +1,18 @@
 module.exports = app => {
-  const user = require("../controllers/user.controller");
-  const admin = require("../controllers/admin.controller");
+  const user = require('../controllers/user.controller')
+  const admin = require('../controllers/admin.controller')
+  app.post('/register_user', (req, res) => {
+    user.regis(req, res)
+  })
+  app.post('/login_user', (req, res) => {
+    user.login(req, res)
+  })
 
-  app.post("/register_user", (req, res) => {
-    user.regis(req, res);
-  });
-  app.post("/login_user", (req, res) => { 
-    user.login(req, res);
-  });
-
-  //=================== admin ====================================
-  app.post("/register_admin", (req, res) => {
-    admin.regis(req, res);
-  });
-  app.post("/login_admin", (req, res) => { 
-    admin.login(req, res);
-  });
-};
+  //= ================== admin ====================================
+  app.post('/register_admin', (req, res) => {
+    admin.regis(req, res)
+  })
+  app.post('/login_admin', (req, res) => {
+    admin.login(req, res)
+  })
+}
